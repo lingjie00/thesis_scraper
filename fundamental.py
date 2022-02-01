d28610b30ad335f1f6aacd808098253ac8c0ee4c
ling <64540764+lingjie00@users.noreply.github.com>
2022-02-01 17:04:13 +0800

    OOP extractors

--- fundamental.py
+++ fundamental.py
@@ -1,37 +1,74 @@
-"""Extract a sample API request from Finage."""
+"""Extract fundamental data from Finage."""
 import logging
 import json
+import csv
 import configparser
 import requests
 
-logging.basicConfig(level=logging.INFO)
 
-config = configparser.ConfigParser()
-config.read("key.ini")
+class Fundamental:
+    # website
+    site = "https://api.finage.co.uk/income-statement/"
 
-# API key
-_API = config["DEFAULT"]["API"]
+    def __init__(self, API, filepath, period="quarter"):
+        self.API = API
+        self.filepath = filepath
+        # period (quarter or annual)
+        self.period = period
 
-# symbol
-symbol = "ULVR.L"
+    def extract(self, symbol):
+        try:
+            # link
+            link = self.site + symbol + ".L" +\
+                "?&period=" + self.period +\
+                "&apikey=" + self.API
+            logging.debug(f"Request link {link}")
+            response = requests.get(link)
 
-# website
-site = "https://api.finage.co.uk/income-statement/"
+            content = response.json()
+            logging.debug(content)
 
-# period (quarter or annual)
-period = "quarter"
+            if "error" in content:
+                raise NotImplementedError(f"{symbol} not found: {content}")
 
-# link
-link = site + symbol + "?&period=" + period + "&apikey=" + _API
+            with open(f"{self.filepath}/{symbol}.json", "w") as file:
+                out = json.dumps(content)
+                file.write(out)
 
-logging.info(f"Request link {link}")
+            logging.info(f"{symbol} fundamental data extracted")
 
-response = requests.get(link)
+        except Exception as e:
+            logging.info(f"{symbol} failed to extract with")
+            logging.info(e)
 
-content = response.json()
+        finally:
+            return self
 
-logging.info(content)
 
-with open(f"output/{symbol}.json", "w") as file:
-    out = json.dumps(content)
-    file.write(out)
+def main():
+    """Run extraction."""
+    logging.basicConfig(filename="fundamental.log", level=logging.INFO)
+
+    config = configparser.ConfigParser()
+    config.read("data/key.ini")
+
+    # API key
+    _API = config["DEFAULT"]["API"]
+
+    # filepath
+    filepath = "output/fundamental"
+
+    extractor = Fundamental(_API, filepath)
+
+    with open("data/LSE_symbols.csv", "r") as file:
+        reader = csv.reader(file, delimiter=",")
+        for row in reader:
+            # symbol stored in first column
+            symbol = row[0]
+            logging.debug(symbol)
+            extractor.extract(symbol=symbol)
+
+
+if __name__ == "__main__":
+    # run extraction
+    main()
--- price.py
+++ price.py
@@ -1,40 +1,79 @@
 """Extract a sample API request from Finage."""
 import logging
 import json
+import csv
 import configparser
 import requests
 
-logging.basicConfig(level=logging.INFO)
 
-config = configparser.ConfigParser()
-config.read("key.ini")
+class Price:
 
-# API key
-_API_KEY = config["DEFAULT"]["API"]
+    # website
+    site = "https://api.finage.co.uk/agg/stock/global/uk/"
 
-# symbol
-symbol = "ULVR"
+    # time
+    time = "1month"  # monthly data
+    start = "1991-01-01"
+    to = "2022-02-01"
 
-# website
-site = "https://api.finage.co.uk/agg/stock/global/uk/"
+    def __init__(self, API, filepath):
+        self.API = API
+        self.filepath = filepath
 
-# time
-time = "1month"  # monthly data
-start = "1991-01-01"
-to = "2021-12-01"
+    def extract(self, symbol):
+        try:
+            # link
+            link = self.site + symbol + "/" +\
+                self.time + "/" +\
+                self.start + "/" + self.to +\
+                "?apikey=" + self.API + "&limit=5000"
 
-# link
-link = site + symbol + "/" + time + "/" + start + "/" + to +\
-    "?apikey=" + _API_KEY + "&limit=5000"
+            logging.debug(f"Request link {link}")
 
-logging.info(f"Request link {link}")
+            response = requests.get(link)
 
-response = requests.get(link)
+            content = response.json()
 
-content = response.json()
+            logging.debug(content)
 
-logging.info(content)
+            if "error" in content:
+                raise NotImplementedError(f"{symbol} not found: {content}")
 
-with open(f"output/{symbol}_price.json", "w") as file:
-    out = json.dumps(content)
-    file.write(out)
+            with open(f"{self.filepath}/{symbol}_price.json", "w") as file:
+                out = json.dumps(content)
+                file.write(out)
+
+            logging.info(f"{symbol} price extracted")
+
+        except Exception as e:
+            logging.info(f"{symbol} failed to extract with")
+            logging.info(e)
+
+        finally:
+            return self
+
+
+def main():
+    """Run price extract"""
+    logging.basicConfig(filename="price.log", level=logging.INFO)
+
+    config = configparser.ConfigParser()
+    config.read("data/key.ini")
+
+    # API key
+    _API_KEY = config["DEFAULT"]["API"]
+
+    extractor = Price(API=_API_KEY, filepath="output/price")
+
+    with open("data/LSE_symbols.csv", "r") as file:
+        reader = csv.reader(file, delimiter=",")
+        for row in reader:
+            # symbol stored in first column
+            symbol = row[0]
+            logging.debug(symbol)
+            extractor.extract(symbol=symbol)
+
+
+if __name__ == "__main__":
+    # run extraction
+    main()
